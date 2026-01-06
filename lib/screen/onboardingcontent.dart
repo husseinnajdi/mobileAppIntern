4a832a270c498fde63d7ebb2692f9d16dd21f3df
unknown <husseinnajdi32@gmail.com>
2026-01-06 11:02:33 +0200

    add the onboarding pages

---
+++ lib/screen/onboardingcontent.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  final String title;
+  final String description;
+  final String image;
+  final VoidCallback onNext;
+  final VoidCallback onSkip;
+  final bool isLast;
+
+  const OnboardingPage({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.onNext,
+    required this.onSkip,
+    this.isLast = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(image,fit: BoxFit.contain,height: 300,width: double.maxFinite,),
+          const SizedBox(height: 40),
+
+          Text(
+            title,
+            style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 40),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: onNext,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  backgroundColor: Colors.blue,
+                ),
+                child: Text(
+                  isLast ? "Get Started" : "Next",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  onSkip();
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+                child: Text("Skip",style: TextStyle(color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,),),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
